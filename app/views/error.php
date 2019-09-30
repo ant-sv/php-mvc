8f3a0eacc64cf2109f33e4d1837d4b9c50f6ff06
ant-sv <45747583+ant-sv@users.noreply.github.com>
2019-09-30 16:44:53 +1000

    Create error.php

---
+++ app/views/error.php
@@ -0,0 +1,5 @@
+            <div class="container p-0">
+                <div class="company-info jumbotron bg-secondary lighten-2 text-white mx-3 mx-sm-0 mt-4 pb-1"> <!-- mx-sm-4 -->
+                    <h6 class="text-center mb-4">Страница не найдена</h6>
+                </div>
+            </div>
