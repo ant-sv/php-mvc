c04ab49fe24ffc416f1eb22a83e4b3615a6a6452
ant-sv <45747583+ant-sv@users.noreply.github.com>
2019-09-30 15:53:54 +1000

    Create config.php

---
+++ app/core/config.php
@@ -0,0 +1,22 @@
+<?php
+
+# dubug mode
+ini_set('display_errors', 0);
+# ini_set('display_errors', 0);
+# error_reporting( E_ALL & ~E_NOTICE & ~E_WARNING );
+
+$CFG = array(
+  
+    'db_host' => 'localhost',
+    'db_name' => '****',
+    'db_user' => '****',
+    'db_pass' => '****',
+    'db_type' => 'mysql',
+    'db_char' => 'utf8',
+
+    'main_menu' => array (
+        'ДОМА В УПРАВЛЕНИИ' => 'manage',
+        'ИНФОРМАЦИЯ'        => 'info',
+        'КОНТАКТЫ'          => 'contacts'
+    )
+);
