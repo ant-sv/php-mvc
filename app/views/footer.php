1ee3d0f03ae8130172465449ab884ad635f9ccef
ant-sv <45747583+ant-sv@users.noreply.github.com>
2019-09-30 16:45:41 +1000

    Create footer.php

---
+++ app/views/footer.php
@@ -0,0 +1,14 @@
+        </div>
+
+        <footer class="footer mdb-color lighten-2">
+            <div class="container text-right text-white small px-2 px-sm-0">
+                <span class="">УК ООО "ЗОЛОТОЙ КЛЮЧ" <?= date('Y'); ?></span>
+            </div>
+        </footer>
+
+        <script src="/public/js/jquery-3.3.1.min.js?"></script>
+        <script src="/public/js/popper.min.js?"></script>
+        <script src="/public/js/bootstrap.min.js?"></script>
+        <script src="/public/js/m.js?"></script>
+    </body>
+</html>
